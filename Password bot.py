4b12f3dc0b024815aadd240e624f2ffa0c820460
DazBagz66 <mr.rozvich@mail.ru>
2025-08-07 15:41:05 +0700

    Bot with 2 buttons

--- Password bot.py
+++ Password bot.py
@@ -1,19 +1,37 @@
 import logging
-from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Application, ContextTypes, CommandHandler, CallbackQueryHandler
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Я ботик, здарова")
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    keyboard = [
+        [
+            InlineKeyboardButton("1-ая кнопка", callback_data="1"),
+            InlineKeyboardButton("2-ая кнопка", callback_data="2")
+        ]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await update.message.reply_text("Выбирай бля", reply_markup=reply_markup)
 
-if __name__ == '__main__':
-    application = ApplicationBuilder().token('8371099251:AAEsSiD1GaC38er7xCQxK2mRVzbDW2-1cgc').build()
+async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    query = update.callback_query
+
+    await query.answer()
+
+    await query.edit_message_text(text=f"Выбранная кнопка: {query.data}")
+
+def main() -> None:
+    application = Application.builder().token('8371099251:AAEsSiD1GaC38er7xCQxK2mRVzbDW2-1cgc').build()
 
     start_handler = CommandHandler('start', start)
+    application.add_handler(CallbackQueryHandler(button))
     application.add_handler(start_handler)
 
     application.run_polling()
+
+if __name__ == '__main__':
+    main()
