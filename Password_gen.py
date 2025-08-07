26eb875e6e848f7128e29a365442eb6738df50fe
DazBags66 <mr.rozvich@mail.ru>
2025-08-08 01:59:57 +0700

    Finished 4 branches of password gen

--- Password_gen.py
+++ Password_gen.py
@@ -12,32 +12,29 @@ def pass_gen(length, is_num, is_spec):
     alphabet = alphabet_num +  alphabet_spec + alphabet_word
 
     """ Ветвления """
-    # Спец знаки и цифры
-    if is_num and is_spec:
-        for i in range(length):
-            str += alphabet[rn.randint(0,len(alphabet)-1)]
-        if (not(re.search(alphabet_num,str))) and ((not(re.search(alphabet_spec,str)))):
-            str = ""
-            for j in range(length-1):
-                str += alphabet[rn.randint(0,len(alphabet)-1)]
-    # Цифры
-    elif ((is_num == True) and (is_spec == False)):
+    if is_num and is_spec:  # Спец знаки и цифры
+         str = ""
+         str += rn.choice(alphabet_spec) + rn.choice(alphabet_num)
+         for i in range(length - 2):
+             str += rn.choice(alphabet)
+             str = ''.join(rn.sample(str,len(str)))
+    elif ((is_num == True) and (is_spec == False)):  # Цифры
         chars_0 = alphabet_word + alphabet_num
-        for i in range(length):
-            str += chars_0[rn.randint(0,len(chars_0)-1)]
-        while bool(re.search(alphabet_spec, str)):
-            str = ""
-            for i in range(length):
-                str += chars_0[rn.randint(0, len(chars_0) - 1)]
-    elif ((is_num == False) and (is_spec == True)):
+        str = ""
+        str += rn.choice(alphabet_num)
+        for i in range(length - 1):
+            str += rn.choice(chars_0)
+            str = ''.join(rn.sample(str,len(str)))
+    elif ((is_num == False) and (is_spec == True)):  # Спец знаки
         chars_1 = alphabet_word + alphabet_spec
         for i in range(length):
             str += chars_1[rn.randint(0,len(chars_1)-1)]
-        while bool(re.search(alphabet_num, str)) and (bool(re.search(alphabet_spec, str)) == False):
+        while bool(re.search(alphabet_spec, str)) == False:
             str = ""
             for i in range(length):
                 str += chars_1[rn.randint(0, len(chars_1) - 1)]
-    # Todo: fix branch
-    return str, bool(re.search(alphabet_spec, str))
-
-print(pass_gen(10,False,True))
+    else:
+        str = ""
+        for i in range(length):
+            str += alphabet_word[rn.randint(0, len(alphabet_word) - 1)]
+    return str
